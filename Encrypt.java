34beaee1721982927c602b8f5b0fd84ea664f2b2
k-k- <kkurashi@calpoly.edu>
2015-03-03 14:36:27 -0800

    The File

---
+++ Encrypt.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Encrypt {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+	public String in(String s, String key) {
+		String code;
+		return code;
+	}
+	public String out(String code, String key) {
+		String s;
+		return s;
+	}
+	private String toBinary(String s, String key) {
+		
+	}
+	private String fromBinary(String s, String key) {
+		
+	}
+	private String addTrash(String s, String key) {
+		
+	}
+	private String cutTrash(String s, String key) {
+		
+	}
+	private String scat(String s, String key) {
+		
+	}
+	private String sort(String s, String key) {
+		
+	}
+}
