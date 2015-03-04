0b235ce768bc553820eb455ae1169f1b192cf1ad
k-k- <kkurashi@calpoly.edu>
2015-03-03 17:36:33 -0800

    The Binary

--- Encrypt/src/Encrypt.java
+++ Encrypt/src/Encrypt.java
@@ -1,7 +1,12 @@
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 public class Encrypt {
 	public static void main(String[] args) {
-		System.out.println();
+		String s = "aφや館";
+		s = toBinary(s);
+		System.out.println(s);
+		s = fromBinary(s);
+		System.out.println(s);
 	}
 	public String in(String s, String key) {
 		String code;
@@ -11,11 +16,29 @@ public class Encrypt {
 		String s;
 		return s;
 	}
-	private String toBinary(String s, String key) {
-		
+	private static String toBinary(String s) {
+		String bin = "";
+		for(int i = 0; i < s.length(); i++) {
+			byte[] b = s.substring(i, i + 1).getBytes(StandardCharsets.UTF_8);
+			for(byte a : b) {
+				bin += String.format("%32s", Integer.toBinaryString(a)).replace(' ', '0').substring(24);
+			}
+		}
+		return bin;
 	}
-	private String fromBinary(String s, String key) {
-		
+	private static String fromBinary(String s) {
+		ArrayList<Byte> l = new ArrayList<Byte>();
+		for(int i = 0; i < s.length(); i += 8) {
+			String bin = s.substring(i, i + 8);
+			short a = (short) Integer.parseInt(bin, 2);
+			byte v = (byte) a;
+			l.add(new Byte(v));
+		}
+		byte[] b = new byte[l.size()];
+		for(int i = 0; i < l.size(); i++) {
+			b[i] = l.get(i).byteValue();
+		}
+		return new String(b, StandardCharsets.UTF_8);
 	}
 	private String addTrash(String s, String key) {
 		
